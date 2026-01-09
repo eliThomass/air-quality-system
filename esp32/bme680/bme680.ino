4217bd13a19f05e07a70c1d0d7a63785b2f5dad3
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2026-01-08 21:16:05 -0800

    added wifi connectivity to bme680

--- esp32/bme680/bme680.ino
+++ esp32/bme680/bme680.ino
@@ -1,9 +1,28 @@
+#include <WiFi.h>
+
+// Wifi data (name & password)
+const char* ssid = "---";
+const char* password = "---";
+
 void setup() {
-  // put your setup code here, to run once:
-  // testing...
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
