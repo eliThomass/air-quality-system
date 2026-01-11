acdbd1cc4616eec5ea44dcc1e04f4ad88b37ebeb
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2026-01-10 17:03:36 -0800

    switching from fastAPI to Django

--- esp32/bme680/bme680.ino
+++ esp32/bme680/bme680.ino
@@ -1,28 +1,38 @@
-#include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
 
-// Wifi data (name & password)
-const char* ssid = "---";
-const char* password = "---";
+Adafruit_BME680 bme; 
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  while (!Serial);
+  Serial.println(F("BME680 Test"));
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // This internaly calls Wire.begin()
+  // We try both common addresses: 0x77 and 0x76
+  if (!bme.begin(0x77) && !bme.begin(0x76)) {
+    Serial.println(F("Could not find sensor. Check wiring!"));
+    while (1);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  // Set up basic gas heater settings
+  bme.setGasHeater(320, 150); 
 }
 
 void loop() {
+  if (!bme.performReading()) {
+    Serial.println(F("Failed to perform reading :("));
+    return;
+  }
 
+  Serial.print(F("Temp: "));
+  Serial.print(bme.temperature);
+  Serial.println(F(" *C"));
+
+  Serial.print(F("Gas: "));
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(F(" KOhms"));
+
+  delay(2000);
 }
